59c749b2e20bc90f450bc8a588f520df653783a1
clemens <dcleme51@gmail.com>
2023-11-28 00:01:57 +0000

    adding an array of questions and creating variables and eventlisteners

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,43 @@
+// GIVEN I am taking a code quiz
+
+// WHEN I click the start button
+var startBtn = document.getElementById("start");
+
+startBtn.addEventListener("click", function() {
+    // console.log("start");
+var startScreen = document.getElementById("start-screen");
+startScreen.style.display = "none";
+
+document.querySelector("#questions").classList.remove("hide")
+renderQs();
+});
+
+var renderQs = function(){
+document.querySelector("#question-title").textContent = questions[0].question
+}
+
+// THEN a timer starts and I am presented with a question
+
+
+// WHEN I answer a question
+
+
+// THEN I am presented with another question
+
+
+// WHEN I answer a question incorrectly
+
+
+// THEN time is subtracted from the clock
+
+
+// WHEN all questions are answered or the timer reaches 0
+
+
+// THEN the game is over
+
+
+// WHEN the game is over
+
+
+// THEN I can save my initials and score
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+        question: 'Question # 1',
+        choices: ['a', 'b', 'c', 'd'],
+        answer: 'b'
+    },
+
+    {
+        question: 'Question # 2',
+        choices: ['a', 'b', 'c', 'd'],
+        answer: 'd'
+    },
+
+    {
+        question: 'Question # 3',
+        choices: ['a', 'b', 'c', 'd'],
+        answer: 'a'
+    },
+
+    {
+        question: 'Question # 4',
+        choices: ['a', 'b', 'c', 'd'],
+        answer: 'c'
+    },
+
+]
