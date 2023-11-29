3a90ab1f36837b7c29ec51f1b39b26c2fb6d32f3
clemens <dcleme51@gmail.com>
2023-11-29 18:32:37 +0000

    working on the timer function

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,23 +1,34 @@
+var timer = document.getElementById("time");
+var totalTime = questions.length*10;
+var timerId;
+
+
 // GIVEN I am taking a code quiz
+function startQuiz(){
 
 // WHEN I click the start button
 var startBtn = document.getElementById("start");
 
-startBtn.addEventListener("click", function() {
-    // console.log("start");
+
 var startScreen = document.getElementById("start-screen");
 startScreen.style.display = "none";
 
 document.querySelector("#questions").classList.remove("hide")
 renderQs();
-});
+};
 
-var renderQs = function(){
+function renderQs(){
 document.querySelector("#question-title").textContent = questions[0].question
-}
+};
 
 // THEN a timer starts and I am presented with a question
+function startTimer(){
+totalTime--;
+timer.textContent =totalTime;
+};
 
+
+// var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 // WHEN I answer a question
 
@@ -41,3 +52,4 @@ document.querySelector("#question-title").textContent = questions[0].question
 
 
 // THEN I can save my initials and score
+document.getElementById("startBtn").addEventListener("click", startQuiz); 
