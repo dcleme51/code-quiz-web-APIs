1a19b8fc86c9153f7497caad9c7881aad4606d6c
clemens <dcleme51@gmail.com>
2023-12-10 10:33:57 +0000

    Working on displaying the right/wrong awnser logic

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,8 +1,9 @@
 var timer = document.getElementById("time");
 // var totalTime = questions.length * 10;
-var totalTime = 5
+var totalTime = 125;
 var timerId;
 var startBtn = document.getElementById("start");
+var feedbackEl = document.getElementById("feedback");
 
 // GIVEN I am taking a code quiz
 function startQuiz() {
@@ -15,13 +16,31 @@ startTimer();
     document.querySelector("#questions").classList.remove("hide")
     renderQs();
 };
-
+function handleAnwser(){
+    //determine if its the correct awnser
+    if(this.value != questions[0].answer){
+        totalTime -= 10;
+        if(totalTime < 0){
+            totalTime = 0;
+        }
+        feedbackEl.textContent="Wrong Awnser!";
+    }
+    else {
+        feedbackEl.textContent="Correct Awnser!"; 
+    }
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function(){
+        feedbackEl.setAttribute("class","feedback hide");
+    }, 2000)
+};
 function renderQs() {
     document.querySelector("#question-title").textContent = questions[0].question
 for(i=0; i < questions[0].choices.length; i++){
 var choiceBtn = document.createElement("button");
 choiceBtn.textContent = questions[0].choices[i];
+choiceBtn.setAttribute("value", questions[0].choices[i]);
 // console.log(choiceBtn);
+choiceBtn.onclick= handleAnwser;
 document.querySelector("#choices").append(choiceBtn);
 }};
 
