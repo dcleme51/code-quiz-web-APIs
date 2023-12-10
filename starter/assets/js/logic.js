d3ddb48323900200608403b2a4fa514d72feec15
clemens <dcleme51@gmail.com>
2023-12-10 11:00:16 +0000

    working on the logic displaying the final highscore

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -4,12 +4,14 @@ var totalTime = 125;
 var timerId;
 var startBtn = document.getElementById("start");
 var feedbackEl = document.getElementById("feedback");
-var index = 0; 
+var index = 0;
+var endDivEl = document.getElementById("end-screen");
+var finalScore = document.getElementById("final-score");
 
 // GIVEN I am taking a code quiz
 function startQuiz() {
-startTimer();
-    
+    startTimer();
+
 
     var startScreen = document.getElementById("start-screen");
     startScreen.style.display = "none";
@@ -17,59 +19,72 @@ startTimer();
     document.querySelector("#questions").classList.remove("hide")
     renderQs();
 };
-function handleAnwser(){
+function handleAnwser() {
     //determine if its the correct awnser
-    if(this.value != questions[0].answer){
+    if (this.value != questions[0].answer) {
         totalTime -= 10;
-        if(totalTime < 0){
+        if (totalTime < 0) {
             totalTime = 0;
         }
-        feedbackEl.textContent="Wrong Awnser!";
+        timer.textContent = totalTime;
+        feedbackEl.textContent = "Wrong Awnser!";
     }
     else {
-        feedbackEl.textContent="Correct Awnser!"; 
+        feedbackEl.textContent = "Correct Awnser!";
     }
     feedbackEl.setAttribute("class", "feedback");
-    setTimeout(function(){
-        feedbackEl.setAttribute("class","feedback hide");
+    setTimeout(function () {
+        feedbackEl.setAttribute("class", "feedback hide");
     }, 2000);
 
     index++;
     //check if you have comepleted the quiz
-    if (index === questions.length){
+    if (index === questions.length) {
         endQuiz();
-    } else{
+    } else {
         renderQs();
     }
 };
-function endQuiz(){
-    // TBC
+
+// WHEN the game is over
+function endQuiz() {
+    //stop timmer 
+    clearInterval(timerId);
+    endDivEl.removeAttribute("class");
+
+    //final score screen
+    finalScore.textContent=totalTime;
+    //hide question sections
+    document.querySelector("#questions").setAttribute("class", "hide");
+
+
 }
 function renderQs() {
     document.querySelector("#question-title").textContent = questions[index].question;
-    document.querySelector("#choices").innerHTML="";
-for(i=0; i < questions[index].choices.length; i++){
-var choiceBtn = document.createElement("button");
-choiceBtn.textContent = questions[index].choices[i];
-choiceBtn.setAttribute("value", questions[index].choices[i]);
-// console.log(choiceBtn);
-choiceBtn.onclick= handleAnwser;
-document.querySelector("#choices").append(choiceBtn);
-}};
+    document.querySelector("#choices").innerHTML = "";
+    for (i = 0; i < questions[index].choices.length; i++) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.textContent = questions[index].choices[i];
+        choiceBtn.setAttribute("value", questions[index].choices[i]);
+        // console.log(choiceBtn);
+        choiceBtn.onclick = handleAnwser;
+        document.querySelector("#choices").append(choiceBtn);
+    }
+};
 
 // THEN a timer starts and I am presented with a question
 function startTimer() {
     timer.textContent = totalTime;
-  timerId = setInterval(function(){
-    if (totalTime > 0){
-        totalTime--
-        timer.textContent = totalTime;
-    
-    } else {
-        clearInterval(timerId);
-    }
+    timerId = setInterval(function () {
+        if (totalTime > 0) {
+            totalTime--
+            timer.textContent = totalTime;
 
-  }, 1000);
+        } else {
+            clearInterval(timerId);
+        }
+
+    }, 1000);
 };
 
 
@@ -92,7 +107,7 @@ function startTimer() {
 // THEN the game is over
 
 
-// WHEN the game is over
+
 
 
 // THEN I can save my initials and score
