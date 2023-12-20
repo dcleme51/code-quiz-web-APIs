917c14e0f74167577a2339a1ed38c996c4bccfa8
clemens <dcleme51@gmail.com>
2023-12-20 18:32:42 +0000

    working on the clear highscores function and debugging

--- starter/assets/js/score.js
+++ starter/assets/js/score.js
@@ -1,12 +1,15 @@
 // creating a even listener to display the highscores
-document.addEventListener("DOMContentLoaded", function () {
-displayHS();
-});
+// document.addEventListener("DOMContentLoaded", function () {
+// displayHS();
+// });
+
 //using local storage to save the highscores
-var scoreSheet = JSON.parse(localStorage.getItem("highScores")) || [];
-var highScores2 = document.getElementById("highscores");
+// var scoreSheet = JSON.parse(window.localStorage.getItem("highScores")) || [];
+// var highScores2 = document.getElementById("highscores");
 
 function displayHS(){
+    var scoreSheet = JSON.parse(window.localStorage.getItem("highScores")) || [];
+var highScores2 = document.getElementById("highscores");
    for(var i = 0; i < scoreSheet.length; i++){
 //save entry from the input of highscores initals
 var listItem = document.createElement("li");
@@ -14,8 +17,20 @@ listItem.textContent = scoreSheet[i].initials + " - " + scoreSheet[i].score;
 
 highscores.appendChild(listItem);
    };
-  
-   
+
+
+
 // console.log(scoreSheet);
 // console.log(scoreSheet[i].score)
 };
+
+  
+// creating a click event, when clearing the highscores is clicked you will restart the quiz.
+function clearHighScores () {
+    window.localStorage.removeItem("scoreSheet");
+    // window.location.reload();
+};
+
+document.getElementById("clear").onclick = clearHighScores;
+
+displayHS();
